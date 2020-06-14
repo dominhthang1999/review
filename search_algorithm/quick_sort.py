15120589783f1b34ea309ae655f0c20914d56a50
Do Minh Thang <44288750+dominhthang1999@users.noreply.github.com>
2020-06-14 15:17:00 +0700

    pivot middle

--- search_algorithm/quick_sort.py
+++ search_algorithm/quick_sort.py
@@ -10,12 +10,31 @@ def partition(arr, low, high):
     arr[i + 1], pivot = pivot, arr[i + 1]
     return i + 1
 
-def quick_sort_last_pivot(arr, low, high):
-    if(low < high):
-        pi = partition(arr, low, high)
-        quick_sort_last_pivot(arr, low, pi - 1)
-        quick_sort_last_pivot(arr, pi + 1, high)
+def partition_middle(arr, low, high):
 
-arr = [10, 80, 30, 90, 40, 50, 70 ]
+    i = low
+    j = high
+    pivot = arr[(low + high) // 2]
+
+    while i <= j:
+        while arr[i] < pivot:
+            i += 1
+
+        while arr[j] > pivot:
+            j -= 1
+        if (i <= j):
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
+    return i
+
+def quick_sort_last_pivot(arr, low, high):
+    if (low < high):
+        pi = partition_middle(arr, low, high)
+        quick_sort_last_pivot(arr, low, pi - 1)
+        quick_sort_last_pivot(arr, pi , high)
+
+
+arr = [1, 12, 5, 26, 7, 14, 3, 7, 2]
 quick_sort_last_pivot(arr, 0, len(arr) - 1)
 print(arr)
